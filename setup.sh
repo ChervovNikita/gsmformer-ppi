03b2f1993f29691f820160a66fd5cf62728f3e76
Nikita Chervov <gcnikitachervov@gmail.com>
2025-07-14 18:47:57 +0000

    libs installing

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip install torch==2.6.0 --index-url https://download.pytorch.org/whl/cu118
+pip install -U torch-geometric -f https://data.pyg.org/whl/torch-2.6.0+cu118.html
+pip install -U torch-optimizer -f https://data.pyg.org/whl/torch-2.6.0+cu118.html
+pip install -U torch-scatter -f https://data.pyg.org/whl/torch-2.6.0+cu118.html
+pip install -U torch-sparse -f https://data.pyg.org/whl/torch-2.6.0+cu118.html
+pip install matplotlib scikit-learn 
